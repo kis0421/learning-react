c0c5fc39d5763288acb981d6e5defa213d27f38a
kis0421 <kig0421@gmail.com>
2022-04-19 20:48:19 +0900

    함수들 state로 구현

--- change-pokemon/src/App.tsx
+++ change-pokemon/src/App.tsx
@@ -1,5 +1,49 @@
 import React from "react";
+import { useSetState } from "react-use";
 
 export default () => {
-    return <>go</>
+
+  const [state, setState] = useSetState({
+    row: 10,
+    column: 10,
+  })
+
+  return <>
+    <div>
+      <header>
+        <section>
+          <div>
+            <span>가로</span>
+            <input type="number" name="row" id="inputRow" value={state.row} />
+          </div>
+          <div>
+            <span>세로</span>
+            <input type="number" name="column" id="inputColumn" value={state.column} />
+          </div>
+        </section>
+        <br />
+        <div>
+          <button onClick={() => setState({ row: state.row + 1 })}>가로 + 1</button>
+          <button onClick={() => setState({ row: state.row - 1 })}>가로 - 1</button>
+
+          <button onClick={() => setState({ column: state.column + 1 })}>세로 + 1</button>
+          <button onClick={() => setState({ column: state.column - 1 })}>세로 - 1</button>
+
+          <button onClick={() => setState({ row: state.row * 2 })}>가로 * 2</button>
+          <button onClick={() => setState({ column: state.column * 2 })}>세로 * 2</button>
+        </div>
+        <br />
+      </header >
+      <br />
+      <section id="pokemonWrap">
+        {new Array(state.row)
+          .fill(false)
+          .map((_, rowIndex) => <div key={rowIndex}>
+            {new Array(state.column)
+              .fill(false)
+              .map((_, columnIndex) => <img src="/image/pikachu.png" key={`${rowIndex}${columnIndex}`} />)}
+          </div>)}
+      </section>
+    </div >
+  </>
 }

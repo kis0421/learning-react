a9914476f0b8660a2f645f458b55e75556667fef
kis0421 <kig0421@gmail.com>
2022-04-19 20:48:02 +0900

    react v18은 createRoot

--- change-pokemon/src/index.tsx
+++ change-pokemon/src/index.tsx
@@ -1,10 +1,10 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-
-ReactDOM.render(
-  <App />,
-  document.getElementById("root") as HTMLElement,
-);
+const rootNode: Element = document.querySelector("#root")!;
+createRoot(rootNode).render(
+  <StrictMode>
+    <App />
+  </StrictMode>);

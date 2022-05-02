5c55a7f5a704d5af793a320167df168f5e904346
kis0421 <kig0421@gmail.com>
2022-05-02 22:40:00 +0900

    y축 돌아가게, 박스 사이즈 수정

--- react-three/src/component/Box.tsx
+++ react-three/src/component/Box.tsx
@@ -5,17 +5,22 @@ const Box = (props: JSX.IntrinsicElements['mesh']) => {
     const ref: any = useRef(null!);
     const [hovered, hover] = useState(false);
     const [clicked, click] = useState(false);
-    useFrame(() => (ref.current.rotation.x += 0.01));
+
+    useFrame(() => {
+
+        ref.current.rotation.x += Math.random() * 0.03;
+        ref.current.rotation.y += Math.random() * 0.03;
+    });
 
     return (
         <mesh
             {...props}
             ref={ref}
             scale={clicked ? 1.5 : 1}
-            onClick={(event) => click(!clicked)}
-            onPointerOver={(event) => hover(true)}
-            onPointerOut={(event) => hover(false)}>
-            <boxGeometry args={[1, 1, 1]} />
+            onClick={() => click(!clicked)}
+            onPointerOver={() => hover(true)}
+            onPointerOut={() => hover(false)}>
+            <boxGeometry args={[2, 2, 2]} />
             <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
         </mesh>
     )

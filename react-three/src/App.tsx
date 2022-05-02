e65b4e7b86f5b49f58561a3603f37d0be68cf5e4
kis0421 <kig0421@gmail.com>
2022-05-02 21:56:24 +0900

    three/fiber 샘플코드

--- react-three/src/App.tsx
+++ react-three/src/App.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Canvas } from '@react-three/fiber'
+import Box from "./component/Box";
 
 export default () => {
-    return <> three </>
+  return <>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  </>
 }

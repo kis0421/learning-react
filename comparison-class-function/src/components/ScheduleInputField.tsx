7632c361db802385eff740127020a158c9e21d37
kis0421 <kig0421@gmail.com>
2022-05-10 02:26:41 +0900

    우선 unControlled inputField

--- comparison-class-function/src/components/ScheduleInputField.tsx
+++ comparison-class-function/src/components/ScheduleInputField.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import ScheduleInputField from "@Styles/ScheduleInputField.style";
 
-export default React.memo(() => {
+export default React.memo((props: { addSchedule: (title: string) => void }) => {
+  const ref = useRef(null);
   return <ScheduleInputField>
-    <input type="text" />
-    <button> + </button>
+    <input ref={ref} type="text" />
+    <button onClick={() => props.addSchedule(ref.current.value)}> + </button>
   </ScheduleInputField>
 })

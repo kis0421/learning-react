381c91afdeac5aba4b5c5b4015074eb120effe2f
kis0421 <kig0421@gmail.com>
2022-05-09 23:37:07 +0900

    Header style 분리

---
+++ comparison-class-function/src/containers/Header.style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default styled.header` 
+    background-color: #F9EBC8;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    color: #777
+`;
--- comparison-class-function/src/containers/Header.tsx
+++ comparison-class-function/src/containers/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import SheduleInputField from "../components/ScheduleInputField";
+import Header from "./Header.style";
+
 
 export default React.memo(() => {
   return <>
-    <header>할 일</header>
+    <Header>할 일</Header>
     <SheduleInputField />
   </>
 })

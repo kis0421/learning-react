844f8d0d68ea5d3573a5c69e176a713f72c44d6b
김민규 <kig0421@gmail.com>
2022-05-16 21:27:57 +0900

    add useRef type

--- comparison-class-function/src/components/ScheduleInputField.tsx
+++ comparison-class-function/src/components/ScheduleInputField.tsx
@@ -2,9 +2,10 @@ import React, { useRef } from "react";
 import ScheduleInputField from "@Styles/ScheduleInputField.style";
 
 export default React.memo((props: { addSchedule: (title: string) => void }) => {
-  const ref = useRef(null);
+  const ref = useRef<HTMLInputElement>(null);
+
   return <ScheduleInputField>
     <input ref={ref} type="text" />
-    <button onClick={() => props.addSchedule(ref.current.value)}> + </button>
+    <button onClick={() => props.addSchedule(ref.current!.value)}> + </button>
   </ScheduleInputField>
 })

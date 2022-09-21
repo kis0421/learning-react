7e9c092a1dad98f737f366af4d3956f9a5e30794
kis0421 <kig0421@gmail.com>
2022-09-21 21:43:19 +0900

    Stars

--- react-three/src/App.tsx
+++ react-three/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Canvas } from '@react-three/fiber'
+import { Stars } from '@react-three/drei'
 import Box from "./component/Box";
 
 export default () => {
   return <>
-    <Canvas>
-      <ambientLight intensity={1}/>
+    <Canvas style={{ position: 'absolute', left: 0, top: 0, }}>
+      <Stars factor={10} radius={50}/>
+      <ambientLight intensity={1} />
       <pointLight position={[10, 10, 10]} />
       <Box position={[-3, 0, 0]} />
       <Box position={[3, 0, 0]} />

df714616b119da6de70954d9b51a34cddeaf4d30
김민규 <kig0421@gmail.com>
2022-10-06 17:23:19 +0900

    client init

---
+++ testing/client/src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+
+const container = document.getElementById("root")!;
+const root = ReactDOM.createRoot(container);
+root.render(<h1>init</h1>);

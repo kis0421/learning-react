39d905c9113ed7744737c6c6378a60c5db5cf140
kis0421 <kig0421@gmail.com>
2022-10-24 18:39:33 +0900

    setList

--- testing/client/src/pages/Main.tsx
+++ testing/client/src/pages/Main.tsx
@@ -1,11 +1,15 @@
-import React from "react"
-
+import React, { useState } from "react"
 interface Props {
   name: string
 }
 
 export default (props: Props) => {
-  return (
+  const [users, setUsers] = useState(["co", "ko"]);
+
+  return (<>
     <h1>hello, {props.name}</h1>
-  )
+    <ul>
+      {users.map((user) => <li key={user}>{user}</li>)}
+    </ul>
+  </>)
 }

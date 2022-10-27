4ff8a613fbc053dc94b17ff51f1d12226b9b22fb
kis0421 <kig0421@gmail.com>
2022-10-27 23:05:30 +0900

    add Main Component test

---
+++ testing/client/src/pages/Main.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen, } from "@testing-library/react";
+
+import Main from "./Main";
+
+it("renders Main Commponent", () => {
+  render(<Main name="hi"/>);
+  expect(screen.getByText("hello, hi")).toBeInTheDocument();
+});

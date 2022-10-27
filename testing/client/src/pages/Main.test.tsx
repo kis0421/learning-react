ac8f07119566e83b625d92317e7a1c5d270db3b7
kis0421 <kig0421@gmail.com>
2022-10-27 23:06:47 +0900

    add descrribe

--- testing/client/src/pages/Main.test.tsx
+++ testing/client/src/pages/Main.test.tsx
@@ -3,7 +3,9 @@ import { render, screen, } from "@testing-library/react";
 
 import Main from "./Main";
 
-it("renders Main Commponent", () => {
-  render(<Main name="hi"/>);
-  expect(screen.getByText("hello, hi")).toBeInTheDocument();
-});
+describe("<Main />", () => {
+  it("renders Main Commponent", () => {
+    render(<Main name="hi" />);
+    expect(screen.getByText("hello, hi")).toBeInTheDocument();
+  });
+})

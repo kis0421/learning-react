90d74783f0176a2d104ad3441a35646f1f57f930
kis0421 <kig0421@gmail.com>
2022-10-27 23:11:18 +0900

    add button temp

--- testing/client/src/pages/Main.test.tsx
+++ testing/client/src/pages/Main.test.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { render, screen, } from "@testing-library/react";
+import { render, screen, fireEvent} from "@testing-library/react";
 
 import Main from "./Main";
 
 describe("<Main />", () => {
+  render(<Main name="hi" />);
+
   it("renders Main Commponent", () => {
-    render(<Main name="hi" />);
     expect(screen.getByText("hello, hi")).toBeInTheDocument();
   });
+  it("click add", () => {
+    // fireEvent.click()
+  });
+
 })

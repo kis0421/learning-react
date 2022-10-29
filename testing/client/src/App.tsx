bd4111fee7df63739f3c1d75b37cbec195d1bd6d
김민규 <kig0421@gmail.com>
2022-10-29 16:05:34 +0900

    add QueryClientProvider

--- testing/client/src/App.tsx
+++ testing/client/src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import Main from "./pages/Main";
 import Footer from "./components/Footer";
 
 interface Props {
-  name: string
+  name: string;
 }
+const queryClient = new QueryClient();
 
 export default (props: Props) => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Main name={props.name} />
       <Footer />
-    </>
-  )
-}
+    </QueryClientProvider>
+  );
+};

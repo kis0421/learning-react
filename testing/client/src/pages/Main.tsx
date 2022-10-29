974341daf3c55412ab1511521cdaaa3cef15765f
김민규 <kig0421@gmail.com>
2022-10-29 16:05:57 +0900

    add style

--- testing/client/src/pages/Main.tsx
+++ testing/client/src/pages/Main.tsx
@@ -1,16 +1,39 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useCallback } from "react";
+import styled from "styled-components";
+
 interface Props {
-  name: string
+  name: string;
 }
+
+const Main = styled.main`
+  display: flex;
+  width: 300px;
+  margin: 0 auto;
+  outline: 1px solid #bebebe;
+  padding: 12px 24px;
+  flex-direction: column;
+`;
 
 export default (props: Props) => {
   const [users, setUsers] = useState(["co", "ko"]);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  return (<>
-    <h1>hello, {props.name}</h1>
-    <button onClick={() => setUsers([...users, new Date().toString()])}>생성</button>
-    <ul>
-      {users.map((user) => <li key={user}>{user}</li>)}
-    </ul>
-  </>)
-}
+  const addUser = useCallback(() => {
+    if (inputRef.current !== null) {
+      setUsers([...users, inputRef.current.value]);
+    }
+  }, [users]);
+
+  return (
+    <Main>
+      <h1>hello, {props.name}</h1>
+      <input type="text" ref={inputRef} />
+      <button onClick={addUser}>생성</button>
+      <ul>
+        {users.map((user) => (
+          <li key={user}>{user}</li>
+        ))}
+      </ul>
+    </Main>
+  );
+};

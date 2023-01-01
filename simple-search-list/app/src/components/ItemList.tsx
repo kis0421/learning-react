99c3f5f5e0c9fd3269220e6c085e235c9d07a87d
kis0421 <kig0421@gmail.com>
2023-01-01 23:21:04 +0900

    Add ItemList

---
+++ simple-search-list/app/src/components/ItemList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ItemListContainer = styled.section`
+  display: flex;
+  background: white;
+  margin-top: 24px;
+  width: 100%;
+  height: 100%;
+`
+
+const ItemList = () => {
+  return (
+    <ItemListContainer>
+
+    </ItemListContainer>
+  )
+}
+
+export default ItemList

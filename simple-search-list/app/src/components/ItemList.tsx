6350c96679fd29030b41eb9b1ed70c1df4599fbe
kis0421 <kig0421@gmail.com>
2023-01-02 00:07:07 +0900

    Add ItemList style

--- simple-search-list/app/src/components/ItemList.tsx
+++ simple-search-list/app/src/components/ItemList.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import styled from 'styled-components';
 import { useQuery } from 'react-query';
 import axios from 'axios';
@@ -7,25 +8,56 @@ const ItemListContainer = styled.section`
   margin-top: 24px;
   width: 100%;
   height: 100%;
+  flex-direction: column;
+  overflow-y: auto;
 `
 interface Props {
   keyword: string
 }
+interface Item {
+  collection: string;
+  datetime: string;
+  display_sitename: string;
+  doc_url: string;
+  height: number;
+  image_url: string;
+  thumbnail_url: string;
+  width: number;
+}
 const ItemList = (props: Props) => {
-  useQuery(['itemList'], () => {
+  const { data, isLoading, refetch } = useQuery<Item[]>(['itemList'], async () => {
     if (props.keyword) {
-      axios.get(`http://dapi.kakao.com/v2/search/image?query=${props.keyword}`, {
-        headers: {
-          Authorization: `KakaoAK ${process.env.REACT_APP_REST_API_KEY}`
-        }
-      }).then(({ data }) => {
-        console.log(data)
-      })
+      try {
+        const { data } = await axios.get(`http://dapi.kakao.com/v2/search/image?query=${props.keyword}`, {
+          headers: {
+            Authorization: `KakaoAK ${process.env.REACT_APP_REST_API_KEY}`
+          }
+        })
+        return data.documents
+      } catch (e) {
+
+      }
     }
   })
+  useEffect(() => {
+    refetch()
+  }, [props.keyword, refetch])
+
+  console.log(data, isLoading)
+  if (data === undefined && isLoading) {
+    return null;
+  }
   return (
     <ItemListContainer>
-
+      {data?.slice(0, 10).map((item) => {
+        return (<img
+          alt={item.display_sitename}
+          key={item.thumbnail_url}
+          src={item.thumbnail_url}
+          width={item.width}
+          height={item.height}
+        />)
+      })}
     </ItemListContainer>
   )
 }
